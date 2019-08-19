69cf511d05670e8df3b9e60f3793c33b5a02360a
vlad-boroda <enity.w@gmail.com>
2019-08-19 23:20:07 +0300

    menu

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import { BenchmarksService } from './services/benchmarksService';
 
 let mainWindow;
@@ -7,7 +7,7 @@ let mainWindow;
 */
 export class Main {
     static main() {
-        this._initElectronListeners();
+        Main._initElectronListeners();
         new BenchmarksService().initListeners();
     }
 
@@ -23,15 +23,43 @@ export class Main {
         });
 
         mainWindow.loadURL(winURL);
-        mainWindow.setMenu(null);
+        mainWindow.setMenu(Main._getMenu());
 
         mainWindow.on('closed', () => {
             mainWindow = null;
         });
     }
 
+    static _getMenu() {
+        const template = [
+            {
+                label: 'Wrk2 Ui',
+                submenu: [
+                    { label: 'About', selector: 'orderFrontStandardAboutPanel:' },
+                    { type: 'separator' },
+                    // eslint-disable-next-line standard/object-curly-even-spacing
+                    { label: 'Quit', accelerator: 'Command+Q', click() { app.quit(); }}
+                ]
+            },
+            {
+                label: 'Edit',
+                submenu: [
+                    { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+                    { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+                    { type: 'separator' },
+                    { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+                    { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+                    { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+                    { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+                ]
+            }
+        ];
+
+        return Menu.buildFromTemplate(template);
+    }
+
     static _initElectronListeners() {
-        app.on('ready', this._createWindow);
+        app.on('ready', Main._createWindow);
 
         app.on('window-all-closed', () => {
             if (process.platform !== 'darwin') {
@@ -41,7 +69,7 @@ export class Main {
 
         app.on('activate', () => {
             if (mainWindow === null) {
-                this.createWindow();
+                Main._createWindow();
             }
         });
     }
